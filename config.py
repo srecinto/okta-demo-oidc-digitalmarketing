1aeb951a03c6b01d79e856a9458ebfd06f724eb9
Shawn Recinto <shawn.recinto@gmail.com>
2017-02-09 15:55:30 +0000

    initial set up of site and back end.  still not connected

---
+++ config.py
@@ -0,0 +1,7 @@
+okta = {
+    "org": "https://recinto.oktapreview.com",
+    "api_token": "00xsNJORlz1G9WoQPhQyFxFUFf0XBe4nHveMH-ZYCm",
+    "oauth_client_id": "rhAAatMJaaMJcWPQiN1v",
+    "oauth_client_secret": "H5Ymwdmr2fWHtcCx24V-Qz-1hpxxBJcCoV6sI7ws",
+    "redirect_uri": "https://okta-demo-oidc-digitalmarketing-recinto.c9users.io/oidc"
+}
