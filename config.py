491a03534a1eed5a9f7473157df4911d75140447
Shawn Recinto <shawn.recinto@gmail.com>
2017-02-09 16:19:28 +0000

    cleared out config setings

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 okta = {
-    "org": "https://recinto.oktapreview.com",
-    "api_token": "00xsNJORlz1G9WoQPhQyFxFUFf0XBe4nHveMH-ZYCm",
-    "oauth_client_id": "rhAAatMJaaMJcWPQiN1v",
-    "oauth_client_secret": "H5Ymwdmr2fWHtcCx24V-Qz-1hpxxBJcCoV6sI7ws",
-    "redirect_uri": "https://okta-demo-oidc-digitalmarketing-recinto.c9users.io/oidc"
+    "org": "",
+    "api_token": "",
+    "oauth_client_id": "",
+    "oauth_client_secret": "",
+    "redirect_uri": ""
 }
